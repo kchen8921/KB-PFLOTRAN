c631f2294a3da811656729630150675ba0e3f1fb
kchen8921 <chenkewei1989@gmail.com>
2020-03-17 11:57:41 -0700

    run pflotran

--- lib/PFLOTRAN/Utils/PFLOTRANUtil.py
+++ lib/PFLOTRAN/Utils/PFLOTRANUtil.py
@@ -54,7 +54,11 @@ class PFLOTRANRunUtil:
         run_pflotran_cmd = exepath + ' -n 1 -pflotranin ' + input_deck_des
         process = subprocess.Popen(run_pflotran_cmd.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
-        print(process.returncode)
+        print("process output:",output)
+        print("process err:",error)
+        pprint(os.listdir(scratch_folder))
+        
+
         output_file = os.path.join(scratch_folder,'batch.h5')
         if os.path.isfile(output_file):
             print ("Successfully run PFLOTRAN")
